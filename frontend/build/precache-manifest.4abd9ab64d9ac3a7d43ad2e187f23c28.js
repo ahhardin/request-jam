af6759862ac44cbbbdc14779fe4f459e797e540e
Ashley <ahhardin@gmail.com>
2018-11-18 10:14:08 -0500

    inital commit

---
+++ frontend/build/precache-manifest.4abd9ab64d9ac3a7d43ad2e187f23c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67caaecc2f824b515390",
+    "url": "/static/js/main.67caaecc.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "67caaecc2f824b515390",
+    "url": "/static/css/main.fd122857.chunk.css"
+  },
+  {
+    "revision": "12e402037d49e0545f1af530c352987c",
+    "url": "/index.html"
+  }
+];
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome to REQUESTjam
+          </p>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
