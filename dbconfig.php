8c1581cadcacd4885902ec12ed49e425e0db364c
Ashley <ahhardin@gmail.com>
2018-11-25 13:12:57 -0500

    changed over to php backend

---
+++ dbconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "sql.requestjam.com";
+
+//Define your database name here.
+$DatabaseName = "requestjam";
+
+//Define your database username here.
+$HostUser = "requestuser";
+
+//Define your database password here.
+$HostPass = "RequestJam";
+
+?>
